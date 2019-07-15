4b24205f902b1da264e464d0290ff3bbab19218d
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-15 14:51:34 -0400

    Added versioning information

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Logo} from './components/Logo/'
 import * as serviceWorker from './services/serviceWorker';
 
 ReactDOM.render(<Logo height="300" width="600" />, document.getElementById('root'));
+ReactDOM.render(<Logo/>, document.getElementById('header'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
