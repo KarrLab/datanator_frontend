ab2901255517362c0cc6fe499bd3384eec5dcb22
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-15 12:14:17 -0400

    starting search components

---
+++ datanator_frontend/src/components/Search/Search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class Search extends Component {
+    constructor(props) {
+        super(props)    
+        this.state = {
+            searchType=this.prop.searchType             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+            </div>
+
+        )
+    }
+}
+
+export {Search}
