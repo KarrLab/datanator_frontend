e7f2f3fd83f1fdfdf3af7f516efa16e3e3b05736
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-16 16:11:49 -0400

    Updated Search component

--- src/components/Search/Search Field/SearchField.js
+++ src/components/Search/Search Field/SearchField.js
@@ -1,24 +1,63 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
+import {
+    Input,
+    AutoComplete,
+} from 'antd';
+import 'antd/dist/antd.css';
 
-export class SearchField extends Component {
+const InputGroup = Input.Group;
+
+class SearchField extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-            
-             
+            type: props.type,
+            name: props.name,
+            dataSource: props.dataSource,
+            value: props.default,
         }
     }
-    
+
     render() {
-        return (
-            <div classname="input field">
-                <form>
-                    <input type="text" id="filter" placeholder="Search for..." ref={input => this.search = input} onChange={this.handleInputChange}/>
-                </form>               
-            </div>
-        )
+
+        if(this.state.type=="AutoComplete"){
+            return (
+                <div className= "Search Field">
+               
+                <AutoComplete
+                dataSource={this.state.dataSource}
+                defaultValue={this.props.default}
+                style={{ width: '30%' }}
+                onSelect={inputValue => { this.setState({ value: inputValue })}}
+                addonBefore="Organism"
+                label="organism"
+                placeholder="Autocomplete here"
+                onChange={inputValue => { this.setState({ value: inputValue })}}
+                filterOption={(inputValue, option) => option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1}>
+                </AutoComplete>
+                </div>
+            )
+        }
+        if (this.state.type=="Input"){
+            return(
+                <div className = "Search Field">
+                    <Input
+                    defaultValue={this.state.default}
+                    style={{ width: '30%' }}
+                    onSelect={inputValue => { this.setState({ value: inputValue })}}
+                    addonBefore="Organism"
+                    label="organism"
+                    placeholder="input here"
+                    onChange={inputValue => { this.setState({ value: inputValue })}}
+                      >
+                          </Input>
+                </div>
+            )
+        }   
     }
 }
 
-export default SearchField
+export {SearchField}
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,7 @@ export class Search extends Component {
     constructor(props) {
         super(props)    
         this.state = {
-            searchType=this.prop.searchType             
+            searchType=this.props.searchType             
         }
     }
     
