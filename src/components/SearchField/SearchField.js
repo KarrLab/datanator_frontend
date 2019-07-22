2518440435f819b8d5bee4e1d9491c67e0b955d6
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-22 12:05:16 -0400

    Abstract search fields

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -1,55 +1,28 @@
-import React, {Component} from 'react';
-import {FormGroup, InputGroup, ControlGroup, Button} from "@blueprintjs/core";
+import React, {Component} from "react";
+import {ControlGroup, Button} from "@blueprintjs/core";
+
+import {MetaboliteInput} from "./MetaboliteInput";
+
 class SearchField extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             type: props.type,
             name: props.name,
             dataSource: props.dataSource,
             value: props.default,
-        }
+        };
     }
 
     render() {
         return(
             <ControlGroup fill='true'>
-        <FormGroup inline="true"
-        helperText= "Search for"
-        label="test"
-        labelFor="text-input"
-        labelInfo="(required)"
-        label="Species"
-        labelFor="species-input"
-        labelInfor="Required"    >
-        <InputGroup id="metabolite-input" placeholder="Placeholder text" />
-    </FormGroup>
-    <FormGroup inline="true"
-        helperText= "Search for"
-        label="test"
-        labelFor="text-input"
-        labelInfo="(required)"
-        label="Species"
-        labelFor="species-input"
-        labelInfor="Required"    >
-        <InputGroup id="metabolite-input" placeholder="Placeholder text" />
-    </FormGroup>
-    <FormGroup inline="true"
-    helperText= "Search for"
-    label="test"
-    labelFor="text-input"
-    labelInfo="(required)"
-    label="Species"
-    labelFor="species-input"
-    labelInfor="Required"    >
-    <InputGroup id="metabolite-input" placeholder="Placeholder text" />
-</FormGroup>
-<Button icon="filter">Filter</Button>
-    </ControlGroup>
+                <MetaboliteInput/>
+                <Button icon="filter">Filter</Button>
+            </ControlGroup>
         );
         
 
-}}
-
+    }}
 export {SearchField};
