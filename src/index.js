80808f11a5621387b7bee391f8e7275a034b5db4
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-22 09:46:33 -0400

    Created Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// Thrid Party Libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+//Styles for @Blueprint JS
 import "./index.css";
 import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
 import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
@@ -7,7 +11,17 @@ import "../node_modules/@blueprintjs/select/lib/css/blueprint-select.css";
 import "../node_modules/@blueprintjs/table/lib/css/table.css";
 import "../node_modules/@blueprintjs/core/lib/scss/variables.scss";
 
-import {Home} from '~/scenes/Home/Home';
+//Website pages (scenes) 
+import {Home} from "~/scenes/Home/Home";
+import {Search} from "~/scenes/Search/Search";
 
+function SiteRouter(){
+    return(
+        <Router> 
+            <Route path="/" exact component={Home}/>
+            <Route path="/search/"component={Search}/>
+        </Router>
+    );
+}
 
-ReactDOM.render(<Home/>,document.getElementById("root"));    
+ReactDOM.render(<SiteRouter/>,document.getElementById("root"));    
