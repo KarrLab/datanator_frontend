3517b09f2d291a9ca760f3ef2408e1d7af376fc8
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-24 17:43:02 -0400

    Added dev and production envs

--- src/components/Results/ResultsTable.js
+++ src/components/Results/ResultsTable.js
@@ -1,170 +1,133 @@
-
-import React, {
-    Component
-} from "react";
-import BootstrapTable from "react-bootstrap-table-next";
-import axios from "axios";
-import "react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css";
+import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
 import filterFactory, {
-    textFilter,
-    selectFilter,
-    numberFilter,
-    Comparator
-} from "react-bootstrap-table2-filter";
-import ReactDOM from "react-dom";
+  textFilter,
+  selectFilter,
+  numberFilter,
+  Comparator,
+} from 'react-bootstrap-table2-filter';
+import ReactDOM from 'react-dom';
 import {
-    Input,
-    Col,
-    Row,
-    Select,
-    InputNumber,
-    DatePicker,
-    AutoComplete,
-    Cascade,
-    Button
-} from "antd";
-import "antd/dist/antd.css";
-import Chart3 from "./Chart3.js";
-import { Slider } from "antd";
-import { withRouter } from "react-router";
-
-import './ResultsTable.css';
-
-
-
-
-
+  Input,
+  Col,
+  Row,
+  Select,
+  InputNumber,
+  DatePicker,
+  AutoComplete,
+  Cascade,
+  Button,
+} from 'antd';
+import Chart3 from './Chart3.js';
+import {styles} from './ResultsTable.css';
 
 const selectRow = {
-    mode: "checkbox",
-    selected: [],
-    onSelect: (row, isSelect, rowIndex, e) => {
-  		console.log(selectRow["selected"]);
-  		row["selected"] = isSelect;
-
-    },
-    onSelectAll: (isSelect, rows, e) => {
-
-        for (var i = rows.length - 1; i >= 0; i--){
-            rows[i]["selected"] = isSelect;
-        }
-    },
+  mode: 'checkbox',
+  selected: [],
+  onSelect: (row, isSelect, rowIndex, e) => {
+    console.log(selectRow['selected']);
+    row['selected'] = isSelect;
+  },
+  onSelectAll: (isSelect, rows, e) => {
+    for (var i = rows.length - 1; i >= 0; i--) {
+      rows[i]['selected'] = isSelect;
+    }
+  },
 };
 
-
-
-
 class ResultsTable extends Component {
-	constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      displayed_columns: [],
+      displayed_data: [],
+      table_size: 0,
+    };
+  }
+  componentDidMount() {
+    this.props.recordDisplayedData(this.node.table);
+  }
 
-        this.state = {
-        	displayed_columns:[],
-            displayed_data:[],
-            table_size:0
+  ooooo_componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.table_size);
+    console.log(this.node.table.props.data.length);
+    if (prevState.table_size !== this.node.table.props.data.length) {
+      this.setState({ table_size: this.node.table.props.data.length });
+      let selected_data = [];
+      let post_filtered_data = this.node.table.props.data;
+      for (var i = post_filtered_data.length - 1; i >= 0; i--) {
+        if (post_filtered_data[i].selected) {
+          selected_data.push(post_filtered_data[i]);
         }
+      }
 
-
+      this.props.recordDisplayedData(selected_data);
+      //this.setState({displayed_data:selected_data})
     }
-    componentDidMount(){
-    	this.props.recordDisplayedData(this.node.table)
+  }
+
+  render() {
+    console.log('woot woot');
+
+    let selected = [];
+    for (var i = this.props.data.length - 1; i >= 0; i--) {
+      if (this.props.data[i]['selected'])
+        selected.push(this.props.data[i]['key']);
+    }
+    selectRow['selected'] = selected;
+
+    let display_columns;
+
+    display_columns = this.props.columns;
+
+    let selected_data = [];
+    for (var i = this.state.displayed_data.length - 1; i >= 0; i--) {
+      if (this.state.displayed_data[i].selected) {
+        selected_data.push(this.state.displayed_data[i]);
+      }
     }
 
-    ooooo_componentDidUpdate(prevProps, prevState) {
-    	console.log(this.state.table_size)
-    	console.log(this.node.table.props.data.length)
-    	 if (prevState.table_size !== this.node.table.props.data.length) {
-    	 	this.setState({table_size:this.node.table.props.data.length})
-	    	let selected_data = []
-	    	let post_filtered_data = this.node.table.props.data;
-	    	for (var i = post_filtered_data.length - 1; i >= 0; i--) {
-			  	if (post_filtered_data[i].selected){
-			  		selected_data.push(post_filtered_data[i]);
-			  	}
-			  }
+    let b_title;
 
-			this.props.recordDisplayedData(selected_data)
-			//this.setState({displayed_data:selected_data})
-		}
-
-
-
+    let next;
+    if (!this.state.advanced) {
+      b_title = 'Advanced';
+      next = true;
+    } else {
+      b_title = 'Basic';
+      display_columns = display_columns.concat(this.props.advanced_columns);
+      console.log(display_columns);
+      next = false;
     }
 
+    return (
+      <div className="concTable2">
+        <img src={require('~/images/result.png')} />
+        <Button
+          type="primary"
+          onClick={event => this.setState({ advanced: next })}
+        >
+          {' '}
+          {b_title}{' '}
+        </Button>
 
-    render() {
-    	console.log("woot woot")
-
-        let selected = [];
-        for (var i = this.props.data.length - 1; i >= 0; i--) {
-            if (this.props.data[i]["selected"])
-                selected.push(this.props.data[i]["key"]);
-        }
-        selectRow["selected"] = selected;
-
-        let display_columns;
-
-        display_columns = this.props.columns;
-
-
-        let selected_data = [];
-		  for (var i = this.state.displayed_data.length - 1; i >= 0; i--) {
-		  	if (this.state.displayed_data[i].selected){
-		  		selected_data.push(this.state.displayed_data[i]);
-		  	}
-		  }
-
-
-
-        let b_title;
-		
-        let next;
-        if (!this.state.advanced){
-            b_title = "Advanced";
-            next = true;
-        }
-        else{
-            b_title = "Basic";
-            display_columns = display_columns.concat(this.props.advanced_columns);
-            console.log(display_columns);
-            next=false;
-        }
-
-
-
-
-	 
-
-
-        return (
-
-		      <div className="concTable2" >
-		      	<img src={require("~/images/result.png")} />
-		      	<Button type="primary" onClick={(event)=>this.setState({advanced:next})}> {b_title} </Button>
-
-
-		      	<div className="bootstrap" >
-		        <BootstrapTable 
-		        ref={ n => this.node = n }
-		        striped
-		        hover
-		        keyField='key' 
-		        data={ this.props.data } 
-		        columns={ display_columns }
-		        filter={ filterFactory() }
-		        selectRow={ selectRow}
-
-		     />
-		     </div>
-
-
-		     </div>
-		)
-    }
+        <div className="bootstrap">
+          <BootstrapTable
+            ref={n => (this.node = n)}
+            striped
+            hover
+            keyField="key"
+            data={this.props.data}
+            columns={display_columns}
+            filter={filterFactory()}
+            selectRow={selectRow}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export {ResultsTable};
-
-
-
+export { ResultsTable };
