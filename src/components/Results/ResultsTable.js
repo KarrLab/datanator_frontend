684a38db8f27c49d3f4468c2e06f2888cd42c354
yosefdroth <yosefdroth@gmail.com>
2019-07-25 16:48:47 -0400

    formatting

--- src/components/Results/ResultsTable.js
+++ src/components/Results/ResultsTable.js
@@ -3,9 +3,7 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
 import filterFactory from 'react-bootstrap-table2-filter';
 import ReactDOM from 'react-dom';
-import {
-  Button,
-} from 'antd';
+import { Button } from 'antd';
 import PropTypes from 'prop-types';
 
 const selectRow = {
@@ -30,52 +28,49 @@ const defaultSorted = [
   },
 ];
 
-
 /**
- * Class to render the results. This table uses react-bootsrap-table2, but adds three additional pieces of functionality 
+ * Class to render the results. This table uses react-bootsrap-table2, but adds three additional pieces of functionality
  * 1) The ability to specify basic and advanced columns, and to toggle between them
  * 2) The addition of a column which allows selection/rejection of data
  * 3) The table keeps track of which data is both displayed and selected. This later gets used to calculated the consensus
  *
  * This file also contains a function which returns the current seleclted data --getSelectedData().
- * This is required to get the current displayed and selected data to calculate the consensus. 
+ * This is required to get the current displayed and selected data to calculate the consensus.
  * The reason this exists as a function here (as opposed to in a parent class) is because the information about
  * which data are displayed exists in the react-bootrsap-table2 component, and can only be accessed by a ref.
  */
 class ResultsTable extends Component {
   static propTypes = {
     /** The data must be a list of dictionaries. Each dictionary cooresponds a single row. Each key is the proper column,
-    * and each value gets entered into the corresponding cell. 
-    * For example:
-    * data = [
-    *   { name: "Adenosine triphosphate", concentration: 4150, units: "uM"},
-    *   { name: "Adenosine triphosphate", concentration: 3829, units: "uM"},
-    * ]
-    */
+     * and each value gets entered into the corresponding cell.
+     * For example:
+     * data = [
+     *   { name: "Adenosine triphosphate", concentration: 4150, units: "uM"},
+     *   { name: "Adenosine triphosphate", concentration: 3829, units: "uM"},
+     * ]
+     */
     data: PropTypes.array.isRequired,
 
-
-    /** This must be a list of columns that can be used in a react-bootstrap-table2 table. The docs for that can be 
-    * found here - https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/getting-started.html
-    * 
-    * This is used to define the columns that show up in the table. Each column represented as a dictionary with 
-    * at least two key:value pairs -- dataField, to the column id; text, to what its title will be upon rendering. 
-    * For example
-    * {
-    *   dataField: 'concentration',
-    *   text: 'Concentration',
-    * }
-    * The dataField id in the columns list must correspond to a key in the data list. 
+    /** This must be a list of columns that can be used in a react-bootstrap-table2 table. The docs for that can be
+     * found here - https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/getting-started.html
+     *
+     * This is used to define the columns that show up in the table. Each column represented as a dictionary with
+     * at least two key:value pairs -- dataField, to the column id; text, to what its title will be upon rendering.
+     * For example
+     * {
+     *   dataField: 'concentration',
+     *   text: 'Concentration',
+     * }
+     * The dataField id in the columns list must correspond to a key in the data list.
      */
     columns: PropTypes.array.isRequired,
 
     /** Optional advanced columns that can be added*/
     advanced_columns: PropTypes.array,
-  }
+  };
 
   constructor(props) {
     super(props);
-
 
     this.state = {
       displayed_data: [],
@@ -95,7 +90,6 @@ class ResultsTable extends Component {
     let data = this.props.data;
     //this.setState({displayed_data:this.props.data})
 
-
     for (var i = data.length - 1; i >= 0; i--) {
       data[i]['key'] = i;
       selectRow['selected'].push(i);
@@ -105,7 +99,6 @@ class ResultsTable extends Component {
 
     this.setState({ displayed_data: data });
   }
-
 
   /**
    * At each render, the selectRow object must be reinitialized with the correct selections from the data.
@@ -184,7 +177,6 @@ class ResultsTable extends Component {
   }
 }
 
-
 /**
  * This returns the data that is both displayed (not filtered out) and selected.
  */
@@ -200,6 +192,5 @@ function getSelectedData() {
   }
   return selected_data;
 }
-
 
 export { ResultsTable, getSelectedData };
