dbc36a229840ff9796130073b082a92419ad081f
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-07-25 19:19:36 -0400

    changed api url

--- src/services/MongoApi.js
+++ src/services/MongoApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const rootSearchURL = process.env.REACT_APP_DATANATOR_REST_SERVER;
 
 function getSearchData(urlParams) {
-  let searchUrl = rootSearchURL + '/results/' + urlParams.join('/');
+  let searchUrl = rootSearchURL + '/search/' + urlParams.join('/');
   return axios.get(searchUrl);
 }
 export { getSearchData };
