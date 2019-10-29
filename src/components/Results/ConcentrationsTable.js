b52cfd44867c425c5b868e3fdc6805d81e74ca9f
yosefdroth <yosefdroth@gmail.com>
2019-10-29 12:01:25 -0400

    refactoring metabconcs

--- src/components/Results/ConcentrationsTable.js
+++ src/components/Results/ConcentrationsTable.js
@@ -25,18 +25,11 @@ import { TanitomoFilter } from '~/components/Results/Filters/TanitomoFilter';
 import { Consensus } from './Consensus.js';
 import { connect } from 'react-redux';
 
-import store from '~/data/Store';
-import {
-  getTotalColumns,
-  filter_taxon,
-  set_lineage,
-  setTotalData,
-} from '~/data/actions/resultsAction';
+
 
 @connect(store => {
   return {
     columns: store.results.columns,
-    //selectedData: store.results.selectedData
   };
 })
 class ConcentrationsTable extends Component {
@@ -48,151 +41,11 @@ class ConcentrationsTable extends Component {
       tanitomo: false,
 
       f_concentrations: [],
-      tanitomo_column: [
-        {
-          dataField: 'tanitomo_similarity',
-          text: 'Tanitomo Score',
-          headerStyle: (colum, colIndex) => {
-            return { width: '20px', textAlign: 'left' };
-          },
-          filter: numberFilter({
-            placeholder: 'custom placeholder',
-            defaultValue: { comparator: Comparator.GE, number: 0.5 }, //ref:this.node,
-            getFilter: filter => (this.tanitomo_filter = filter),
-          }),
-        },
-      ],
-    };
 
-    this.formatData = this.formatData.bind(this);
+    };
     this.handleUpdate = this.handleUpdate.bind(this);
   }
 
-  formatData(data) {
-    if (data != null) {
-      var f_concentrations = [];
-
-      this.props.dispatch(set_lineage(data[2][0]));
-
-      for (var n = data[0].length; n > 0; n--) {
-        if (data[0][n - 1].tanitomo_similarity < 1) {
-          this.setState({ tanitomo: true });
-        } else {
-          this.setState({ tanitomo: false });
-        }
-
-        var concs = data[0][n - 1].concentrations;
-        if (concs != null) {
-          if (!Array.isArray(concs.concentration)) {
-            for (var key in concs) {
-              // check if the property/key is defined in the object itself, not in parent
-              if (concs.hasOwnProperty(key)) {
-                concs[key] = [concs[key]];
-              }
-            }
-          }
-          for (var i = concs.concentration.length - 1; i >= 0; i--) {
-            var growth_phase = '';
-            var organism = 'Escherichia coli';
-
-            if (concs.growth_status[i] != null) {
-              if (
-                concs.growth_status[i].toLowerCase().indexOf('stationary') >= 0
-              ) {
-                growth_phase = 'Stationary Phase';
-              } else if (
-                concs.growth_status[i].toLowerCase().indexOf('log') >= 0
-              ) {
-                growth_phase = 'Log Phase';
-              }
-            }
-            if ('strain' in concs) {
-              if (concs.strain != null) {
-                if (concs.strain[i] != null) {
-                  organism = organism + ' ' + concs.strain[i];
-                }
-              }
-            }
-
-            f_concentrations.push({
-              name: data[0][n - 1].name,
-              concentration: parseFloat(concs.concentration[i]),
-              units: concs.concentration_units[i],
-              error: concs.error[i],
-              growth_phase: growth_phase,
-              organism: organism,
-              growth_conditions: concs.growth_system[i],
-              growth_media: concs.growth_media[i],
-              taxonomic_proximity: data[0][n - 1].taxon_distance,
-              tanitomo_similarity: data[0][n - 1].tanitomo_similarity,
-            });
-          }
-        }
-      }
-
-      for (var n = data[1].length; n > 0; n--) {
-        if (data[1][n - 1].tanitomo_similarity < 1) {
-          this.setState({ tanitomo: true });
-        }
-
-        var concs = data[1][n - 1].concentrations;
-        if (concs != null) {
-          if (!Array.isArray(concs.concentration)) {
-            for (var key in concs) {
-              // check if the property/key is defined in the object itself, not in parent
-              if (concs.hasOwnProperty(key)) {
-                concs[key] = [concs[key]];
-              }
-            }
-          }
-          for (var i = concs.concentration.length - 1; i >= 0; i--) {
-            var growth_phase = '';
-            var organism = data[1][n - 1].species;
-            if ('strain' in concs) {
-              if (concs.strain != null) {
-                if (concs.strain[i] != null) {
-                  organism = organism + ' ' + concs.strain[i];
-                }
-              }
-            }
-
-            f_concentrations.push({
-              name: data[1][n - 1].name,
-              concentration: parseFloat(concs.concentration[i]),
-              units: concs.concentration_units[i],
-              error: concs.error[i],
-              growth_phase: growth_phase,
-              organism: organism,
-              growth_media: concs.growth_media[i],
-              taxonomic_proximity: data[1][n - 1].taxon_distance,
-              tanitomo_similarity: data[1][n - 1].tanitomo_similarity,
-            });
-          }
-        }
-      }
-
-      this.setState({
-        f_concentrations: f_concentrations,
-        //displayed_data: f_concentrations
-      });
-      this.props.dispatch(setTotalData(f_concentrations));
-    } else {
-      //alert('Nothing Found');
-    }
-  }
-
-  componentDidMount() {
-    if ((this.props.json_data)){
-      this.formatData(this.props.json_data);
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps.json_data)
-    if (this.props.json_data != prevProps.json_data){
-      this.formatData(this.props.json_data);
-    }
-  }
 
   handleUpdate() {
     this.setState({
@@ -202,51 +55,44 @@ class ConcentrationsTable extends Component {
     //this.props.dispatch(filter_taxon(3))
   }
 
-  handleSlider(value) {
-    //this.filter_taxon;
-  }
-
-
 
   render() {
-    console.log("Rendering ConcentrationsTable")
-    let basic_columns
+    console.log('Rendering ConcentrationsTable');
 
     if (!this.props.data_arrived) {
-      return(<div></div>)
+      return <div></div>;
+    } else {
+      return (
+        <div className="total_table">
+          <div className="slider">
+            <TaxonFilter />
+            <TanitomoFilter tanitomo={this.props.tanitomo} />
+          </div>
+          <div className="results">
+            <div className="concTable">
+              <ResultsTable
+                basic_columns={[
+                  'concentration',
+                  'error',
+                  'molecule',
+                  'organism',
+                  'taxonomic_proximity',
+                ]}
+                advanced_columns={[
+                  'growth_phase',
+                  'growth_conditions',
+                  'growth_media',
+                ]}
+                potential_columns={{ tanitomo: this.props.tanitomo }}
+              />
+            </div>
+            <div className="consensus">
+              <Consensus relevantColumn={'concentration'} />
+            </div>
+          </div>
+        </div>
+      );
     }
-    else{
-    return (
-      <div className="total_table">
-        <div className="slider">
-          <TaxonFilter />
-          <TanitomoFilter tanitomo={this.props.tanitomo} />
-        </div>
-        <div className="results">
-          <div className="concTable">
-            <ResultsTable
-              basic_columns={[
-                'concentration',
-                'error',
-                'molecule',
-                'organism',
-                'taxonomic_proximity',
-              ]}
-              advanced_columns={[
-                'growth_phase',
-                'growth_conditions',
-                'growth_media',
-              ]}
-              potential_columns={{'tanitomo':this.props.tanitomo}}
-            />
-          </div>
-          <div className="consensus">
-            <Consensus relevantColumn={'concentration'} />
-          </div>
-        </div>
-      </div>
-    );
-}
   }
 }
 
