c43524de0ae5ad7ba7f2430aad9e5f2a9fd0aaeb
yosefdroth <yosefdroth@gmail.com>
2019-11-04 09:33:31 -0500

    docs

--- src/components/Results/ProtAbundancesTable.js
+++ src/components/Results/ProtAbundancesTable.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
-//import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
-import filterFactory, {
-  numberFilter,
-  Comparator,
-} from 'react-bootstrap-table2-filter';
 import 'antd/dist/antd.css';
 import { withRouter } from 'react-router';
-
 import './ConcentrationsTable.css';
-
 import { ResultsTable } from './components/ResultsTable.js';
-//import { getTotalColumns } from './Columns2.js';
-
 import { TaxonFilter } from '~/components/Results/components/Filters/TaxonFilter';
-
 import { Consensus } from './components/Consensus.js';
 import { connect } from 'react-redux';
-
-import { setTotalData } from '~/data/actions/resultsAction';
 
 @connect(store => {
   return {
@@ -25,49 +13,52 @@ import { setTotalData } from '~/data/actions/resultsAction';
   };
 })
 class ProtAbundancesTable extends Component {
+  static propTypes = {
+    /**
+     * Value to keep track of whether the JSON data has arrived
+     * If this value is false, then the render should return a blank div
+     */
+    data_arrived: PropTypes.bool.isRequired,
+  };
   constructor(props) {
     super(props);
-
   }
 
   render() {
-    let basic_columns
-    console.log("Rendering ProtAbundancesTable")
+    console.log('Rendering ProtAbundancesTable');
 
     if (!this.props.data_arrived) {
-      return(<div></div>)
+      return <div></div>;
+    } else {
+      return (
+        <div className="total_table">
+          <div className="slider">
+            <img src={require('~/images/filter.png')} alt="results" />
+            <TaxonFilter />
+          </div>
+          <div className="results">
+            <div className="concTable">
+              <ResultsTable
+                basic_columns={[
+                  'protein_name',
+                  'uniprot_id',
+                  'gene_symbol',
+                  'abundance',
+                  'organism',
+                  'taxonomic_proximity',
+                  'organ',
+                ]}
+                advanced_columns={[]}
+                potential_columns={{}}
+              />
+            </div>
+            <div className="consensus">
+              <Consensus relevantColumn={'abundance'} />
+            </div>
+          </div>
+        </div>
+      );
     }
-    else{
-    return (
-      <div className="total_table">
-        <div className="slider">
-        <img src={require('~/images/filter.png')} alt="results" />
-          <TaxonFilter />
-        </div>
-        <div className="results">
-          <div className="concTable">
-            <ResultsTable
-              basic_columns={[
-                'protein_name',
-                'uniprot_id',
-                'gene_symbol',
-                'abundance',
-                'organism',
-                'taxonomic_proximity',
-                'organ',
-              ]}
-              advanced_columns={[
-              ]}
-              potential_columns={{}}
-            />
-          </div>
-          <div className="consensus">
-            <Consensus relevantColumn={'abundance'} />
-          </div>
-        </div>
-      </div>
-    );
-  }
   }
 }
 
