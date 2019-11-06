c259602b28ab410b30ff9334a64ba9a1ec8e65bd
yosefdroth <yosefdroth@gmail.com>
2019-11-06 11:59:50 -0500

    refactoring css

--- src/scenes/Home/HomeOld.js
+++ src/scenes/Home/HomeOld.js
@@ -1,53 +1,41 @@
 // App.js
 //import './App.css';
 
-import React, {
-	Component
-} from 'react';
+import React, { Component } from 'react';
 //import '~/../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import ConcSearch from '~/components/SearchField/ConcSearch.js';
 import ProtSearch from '~/components/SearchField/ProtSearch.js';
 
-import {BrowserRouter, Redirect } from 'react-router-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { withRouter } from "react-router";
+import { BrowserRouter, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-//import { setNewUrl } from '~/data/actions/pageAction';
-import {Header} from '~/components/Layout/Header/Header';
-
+import { Header } from '~/components/Layout/Header/Header';
 import '~/scenes/Home/HomeOld.css';
-
-
-
 class HomeOld extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			dataSource: [],
-			newSearch:false,
-			currentSearch: "metab",
-			nextUrl:"",
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+      newSearch: false,
+      currentSearch: 'metab',
+      nextUrl: '',
+    };
+    this.getSearchDataProt = this.getSearchDataProt.bind(this);
+    this.getNewSearchMetab = this.getNewSearchMetab.bind(this);
+  }
 
-		this.getSearchDataProt = this.getSearchDataProt.bind(this);
-		this.getNewSearchMetab = this.getNewSearchMetab.bind(this);
-	}
+  getSearchDataProt(url) {
+    this.setState({ nextUrl: url });
+    console.log(url);
+    this.setState({ newSearch: true });
+  }
 
+  componentDidMount() {
+    this.props.history.push('/');
+  }
 
-	getSearchDataProt(url) {
-		this.setState({nextUrl:url})
-		console.log(url)
-		this.setState({newSearch:true})
-		//this.props.dispatch(setNewUrl(url))
-
-	}
-
-	componentDidMount(){
-		this.props.history.push("/");
-	}
-
-	getNewSearchMetab(response) {
-
+  getNewSearchMetab(response) {
     let url = '/metabconcs/' + response[0] + '/' + response[1];
     if (url !== this.state.new_url) {
       this.setState({ nextUrl: url });
@@ -55,59 +43,54 @@ class HomeOld extends Component {
     }
   }
 
-
-
-
-	render() {
-
-	if (this.state.newSearch == true) {
+  render() {
+    if (this.state.newSearch == true) {
       return <Redirect to={this.state.nextUrl} push />;
     }
 
-
     return (
+      <Grid fluid>
+        <Row center="xs">
+          <Col xs={'100%'}>
+            {this.state.currentSearch == 'metab' && (
+              <ConcSearch handleClick={this.getNewSearchMetab} landing={true} />
+            )}
+            {this.state.currentSearch == 'protein' && (
+              <ProtSearch handleClick={this.getSearchDataProt} landing={true} />
+            )}
+          </Col>
+        </Row>
 
-    <Grid fluid>
-    	
-    	<Row center="xs">
-    	<Col  xs={"100%"}>
-    	{this.state.currentSearch == "metab" &&
-	        <ConcSearch  handleClick={this.getNewSearchMetab} landing={true}/>
-	    }
-	    {this.state.currentSearch == "protein" &&
-	        <ProtSearch  handleClick={this.getSearchDataProt} landing={true}/>
-	    }
-	        </Col>
-	     </Row>
-	      
+        <Row center="xs" bottom="xs">
+          <Col xs={4}>
+            <img
+              src={require('~/scenes/Home/images/molecule.png')}
+              style={{ width: '50px' }}
+              onClick={() => this.setState({ currentSearch: 'metab' })}
+            />
+            <p>Metabolite Concentrations</p>
+          </Col>
 
-		<Row center="xs" bottom="xs">
-		<Col xs={4} >
-		        <img src={require('~/scenes/Home/images/molecule.png')} style={{ width: '50px' }}
-		     onClick={() => this.setState({currentSearch:"metab"})} />
-		        <p>Metabolite Concentrations</p>
+          <Col xs={4}>
+            <img
+              src={require('~/scenes/Home/images/protein.png')}
+              style={{ width: '60px' }}
+              onClick={() => this.setState({ currentSearch: 'protein' })}
+            />
+            <p>Protein Concentrations</p>
+          </Col>
 
-		</Col>
-
-		<Col xs={4} >
-		        <img src={require('~/scenes/Home/images/protein.png')} style={{ width: '60px' }}
-		        onClick={() => this.setState({currentSearch:"protein"})} />
-		        <p>Protein Concentrations</p>
-
-		</Col>
-
-		<Col xs={4}>
-		        <img src={require('~/scenes/Home/images/reaction.png')} style={{ width: '100px' }}/>
-		        <p>Reaction Kinetics</p>
-		</Col>
-
-	</Row>
-	      
-</Grid>
-
-
+          <Col xs={4}>
+            <img
+              src={require('~/scenes/Home/images/reaction.png')}
+              style={{ width: '100px' }}
+            />
+            <p>Reaction Kinetics</p>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
 
-export default (HomeOld);
+export default HomeOld;
