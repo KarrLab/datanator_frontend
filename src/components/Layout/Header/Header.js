b0f9997b92a92a1c64fd9b6cc411585c5e984281
yosefdroth <yosefdroth@gmail.com>
2019-12-20 10:31:51 -0500

    adding search bar to header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -5,11 +5,22 @@ import { Logo } from '~/components/Layout/Logo';
 
 const Header = () => {
   return (
-    <Navbar fixedToTop="true" className="bp3-dark navbar">
+     <Navbar fixedToTop="true" className="bp3-dark navbar">
       <Navbar.Group className="logo-holder">
         <Logo className="logo" />
       </Navbar.Group>
-
+      <Navbar.Group className="searchbar">
+        <InputGroup
+          className="searchbar-input"
+          leftIcon="search"
+          placeholder="Search for..."
+        />
+        <InputGroup
+          className="searchbar-input"
+          //leftIcon="search"
+          placeholder="In organism..."
+        />
+      </Navbar.Group>
       <Navbar.Group align className="page-links">
         <AnchorButton
           minimal="true"
@@ -18,9 +29,29 @@ const Header = () => {
           text="Home"
           href="/"
         />
-
+        <AnchorButton
+          minimal="true"
+          className="navbutton"
+          icon="info-sign"
+          text="About"
+          href="/about/"
+        />
+        <AnchorButton
+          minimal="true"
+          className="navbutton"
+          icon="search"
+          text="Search"
+          href="/search/"
+        />
+        <AnchorButton
+          minimal="true"
+          className="navbutton"
+          icon="document"
+          text="Data"
+          href="/data/"
+        />
       </Navbar.Group>
-    </Navbar>
+</Navbar>
   );
 };
 export { Header };
