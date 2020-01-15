ecdd15ef8ce887a998ab7659249e432ba64e271a
Jonathan Karr <jonrkarr@gmail.com>
2020-01-15 18:28:54 -0500

    organizing header styles; simplifying logo DOM; boldening text in top bar; increasing contrast of color changes on hovering over links in top menu

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -2,15 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 const Logo = props => {
   return (
-    <div className="logo">
-      <a href="/">
-        <img
-          src={logo}
-          className="logo-image"
-          alt="Datanator Logo"
-        />
-      </a>
-    </div>
+    <a href="/" className="logo">
+      <img
+        src={logo}
+        className="logo-image"
+        alt="Datanator Logo"
+      />
+    </a>
   );
 };
 export { Logo };
