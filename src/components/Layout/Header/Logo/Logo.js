09bea008e3981f40e2ab92dfba7920255e132517
Jonathan Karr <jonrkarr@gmail.com>
2020-01-16 15:15:18 -0500

    adding title and subtitle to top bar

--- src/components/Layout/Header/Logo/Logo.js
+++ src/components/Layout/Header/Logo/Logo.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-const Logo = props => {
+import React from "react";
+import logo from "./logo.svg";
+const Logo = () => {
   return (
     <a href="/" className="logo-inner-container">
-      <img
-        src={logo}
-        className="logo"
-        alt="Datanator Logo"
-      />
+      <img src={logo} className="logo" alt="Datanator Logo" />
     </a>
   );
 };
