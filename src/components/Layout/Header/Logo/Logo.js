352ac0b70ed539998180028587371c68c5153833
Jonathan Karr <jonrkarr@gmail.com>
2020-01-16 21:40:27 -0500

    encapsulting logo styles

--- src/components/Layout/Header/Logo/Logo.js
+++ src/components/Layout/Header/Logo/Logo.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Logo.scss";
 import logo from "./logo.svg";
 const Logo = () => {
   return (
-    <a href="/" className="logo-inner-container">
-      <img src={logo} className="logo" alt="Datanator Logo" />
-    </a>
+    <Link to="/" className="logo">
+      <img src={logo} className="logo-image" alt="Datanator Logo" />
+    </Link>
   );
 };
 export { Logo };
