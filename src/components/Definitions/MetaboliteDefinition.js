ca6f2c11bc84e21358c60b412218b8c343773cf3
Jonathan Karr <jonrkarr@gmail.com>
2020-01-17 16:21:23 -0500

    removing non-sensical comment

--- src/components/Definitions/MetaboliteDefinition.js
+++ src/components/Definitions/MetaboliteDefinition.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
--- src/scenes/About/About.js
+++ src/scenes/About/About.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { withRouter } from "react-router";
