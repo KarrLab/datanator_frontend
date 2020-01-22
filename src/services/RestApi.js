32ca22c7fb2bd914aebf308c2515611a3fc42da7
Jonathan Karr <jonrkarr@gmail.com>
2020-01-22 17:19:08 -0500

    simplifying

--- src/services/RestApi.js
+++ src/services/RestApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const rootSearchURL = process.env.REACT_APP_REST_SERVER;
+const rootURL = process.env.REACT_APP_REST_SERVER;
 
-function getDataFromApi(urlParams, options = {}) {
-  let searchUrl = rootSearchURL + urlParams.join("/");
-  //console.log(searchUrl)
-  return axios.get(searchUrl, options);
+function getDataFromApi(params, options = {}) {
+  let url = rootURL + params.join("/");
+  //console.log(url)
+  return axios.get(url, options);
 }
 
 export { getDataFromApi };
