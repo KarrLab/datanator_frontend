1df0836568075e234235da928366044f9ff7a28c
Jonathan Karr <jonrkarr@gmail.com>
2020-01-22 11:29:36 -0500

    moving location of REST API to environment variable

--- src/services/MongoApi.js
+++ src/services/MongoApi.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-//const rootSearchURL = process.env.REACT_APP_DATANATOR_REST_SERVER;
-const rootSearchURL = "https://api.datanator.info/";
+const rootSearchURL = process.env.REACT_APP_REST_SERVER;
 
 function getDataFromApi(urlParams, options = {}) {
   let searchUrl = rootSearchURL + urlParams.join("/");
-  console.log(searchUrl)
   return axios.get(searchUrl, options);
 }
 
