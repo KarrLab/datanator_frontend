13633ac087e2e7c85512108bd5904e2134d4db89
Jonathan Karr <jonrkarr@gmail.com>
2020-01-23 12:55:00 -0500

    improving dialog for REST errors

--- src/services/RestApi.js
+++ src/services/RestApi.js
@@ -6,22 +6,23 @@ const ROOT_URL = process.env.REACT_APP_REST_SERVER;
 const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 const DEFAULT_ERROR_MESSAGE = (
   <span>
-    We&apos;re sorry. Our server could not complete your request. Please try
+    We&apos;re sorry our server could not complete your request. Please try
     again, or contact us at{" "}
     <a href="mailto:info@karrlab.org">info@karrlab.org</a> if the problem
     persists.
   </span>
 );
 
-function getDataFromApi(
-  params,
-  options = {},
-  errorMessage = DEFAULT_ERROR_MESSAGE
-) {
+function getDataFromApi(params, options = {}, errorMessage = null) {
   const url = ROOT_URL + params.join("/");
   return axios.get(url, options).catch(error => {
     if (!axios.isCancel(error)) {
-      errorDialogRef.current.open(errorMessage);
+      errorDialogRef.current.open(
+        <span className="dialog-message-container">
+          {errorMessage && <span>{errorMessage}</span>}
+          {DEFAULT_ERROR_MESSAGE}
+        </span>
+      );
       if (IS_DEVELOPMENT) {
         console.log("Server error", error);
       }
