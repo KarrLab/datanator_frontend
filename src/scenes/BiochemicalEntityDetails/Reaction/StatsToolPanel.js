172df7bc7b6b4636ab788f8850e38148bd900e85
Jonathan Karr <jonrkarr@gmail.com>
2020-01-23 00:10:46 -0500

    simplifying stats tool panels and beginning to standardize implementation

--- src/scenes/BiochemicalEntityDetails/Reaction/StatsToolPanel.js
+++ src/scenes/BiochemicalEntityDetails/Reaction/StatsToolPanel.js
@@ -1,45 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { StatsToolPanel as BaseStatsToolPanel } from "../StatsToolPanel/StatsToolPanel";
 
 export default class StatsToolPanel extends Component {
+  static propTypes = {
+    "relevant-column": PropTypes.string
+  };
+
   constructor(props) {
     super(props);
 
-    this.state = { numMedals: 0, numGold: 0, numSilver: 0, numBronze: 0 };
-
     // calculate stats when new rows loaded, i.e. onModelUpdated
-    //this.props.api.addEventListener('modelUpdated', this.updateTotals.bind(this));
+    // this.props.api.addEventListener(
+    //   "modelUpdated",
+    //   this.calcStats.bind(this)
+    // );
   }
-
 
   render() {
     return (
-      <BaseStatsToolPanel
-        relevantColumns={[this.props.relevant_column]}
-        optional_columns={[]}
-      />
+      <BaseStatsToolPanel relevant-column={this.props["relevant-column"]} />
     );
   }
 
-  updateTotals() {
-    var numGold = 0,
-      numSilver = 0,
-      numBronze = 0;
-
-    this.props.api.forEachNode(function(rowNode) {
-      let data = rowNode.data;
-
-      if (data.gold) numGold += data.gold;
-      if (data.silver) numSilver += data.silver;
-      if (data.bronze) numBronze += data.bronze;
-    });
-
-    let numMedals = numGold + numSilver + numBronze;
-    this.setState({
-      numMedals: numMedals,
-      numGold: numGold,
-      numSilver: numSilver,
-      numBronze: numBronze
-    });
-  }
+  // calcStats() {}
 }
--- src/scenes/BiochemicalEntityDetails/Rna/StatsToolPanel.js
+++ src/scenes/BiochemicalEntityDetails/Rna/StatsToolPanel.js
@@ -5,43 +5,16 @@ export default class StatsToolPanel extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { numMedals: 0, numGold: 0, numSilver: 0, numBronze: 0 };
-
     // calculate stats when new rows loaded, i.e. onModelUpdated
-    this.props.api.addEventListener(
-      "modelUpdated",
-      this.updateTotals.bind(this)
-    );
+    // this.props.api.addEventListener(
+    //   "modelUpdated",
+    //   this.calcStats.bind(this)
+    // );
   }
 
   render() {
-    return (
-      <BaseStatsToolPanel
-        relevantColumns={["abundance"]}
-        optional_columns={[]}
-      />
-    );
+    return <BaseStatsToolPanel relevant-column="abundance" />;
   }
 
-  updateTotals() {
-    var numGold = 0,
-      numSilver = 0,
-      numBronze = 0;
-
-    this.props.api.forEachNode(function(rowNode) {
-      let data = rowNode.data;
-
-      if (data.gold) numGold += data.gold;
-      if (data.silver) numSilver += data.silver;
-      if (data.bronze) numBronze += data.bronze;
-    });
-
-    let numMedals = numGold + numSilver + numBronze;
-    this.setState({
-      numMedals: numMedals,
-      numGold: numGold,
-      numSilver: numSilver,
-      numBronze: numBronze
-    });
-  }
+  // calcStats() { }
 }
