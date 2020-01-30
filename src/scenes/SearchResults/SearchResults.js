1e54eda3dc05d6c015559e1dcbb2c9621dcb1b7e
yosefdroth <yosefdroth@gmail.com>
2020-01-30 16:37:19 -0500

    solving hash loading bug

--- src/scenes/SearchResults/SearchResults.js
+++ src/scenes/SearchResults/SearchResults.js
@@ -10,6 +10,7 @@ import ReactionSearchResultsList from "./ReactionSearchResultsList";
 import "./SearchResults.scss";
 
 class SearchResults extends Component {
+  
   render() {
     return (
       <div className="content-container content-container-search-results-scene">
