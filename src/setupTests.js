2b61458bdd62622d2ad250fedc09b4106fe51ea8
Jonathan Karr <jonrkarr@gmail.com>
2020-02-10 11:49:13 -0500

    adding enzyme utilities for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,7 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 window.URL.createObjectURL = function() {};
+
+// setup Enzyme
+configure({ adapter: new Adapter() });
