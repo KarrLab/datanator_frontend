f606ad5f525e1acf30fd87b83a2f02629fb6054f
Jonathan Karr <jonrkarr@gmail.com>
2020-02-10 11:13:33 -0500

    updating REST API test

--- src/__tests__/services/RestApi.test.js
+++ src/__tests__/services/RestApi.test.js
@@ -1,29 +1,47 @@
-import { getDataFromApi, getSearchObject } from '~/services/RestApi';
+/**
+ * @jest-environment jsdom
+ */
 
-test.skip('runs', () => {
-  let result = getDataFromApi(['ATP', 'Escherichia coli']);
-  console.log(result);
-  return result.then(response => {
-    expect(response.data).toBeTruthy;
+import { getDataFromApi } from "~/services/RestApi";
+import axios from "axios";
+import { ErrorDialog } from "~/components/ErrorDialog/ErrorDialog";
+import React from "react";
+import { render } from "@testing-library/react";
+
+jest.mock("axios");
+
+test("Request successful", async () => {
+  axios.get.mockResolvedValue({
+    status: 200,
+    data: { message: "Api server is up." }
+  });
+
+  await getDataFromApi(["status"]).then(response => {
     expect(response.status).toBe(200);
+    expect(response.data.message).toBe("Api server is up.");
   });
 });
 
-test.skip('Fails on bad search', () => {
-  return getDataFromApi(['ATPa', 'Escherichia coli']).then(response => {
-    expect(response.data).toBeFalsey;
-    expect(response.status).toBe(500);
+test("Request failed", async () => {
+  axios.get.mockRejectedValue({
+    response: { status: 404, data: { status: 404, detail: "Server error." } }
   });
+
+  const errorDialogRef = React.createRef();
+  render(<ErrorDialog ref={errorDialogRef} />);
+  console.log = jest.fn();
+  await getDataFromApi(["not-implemented"], {}, null, errorDialogRef);
+  expect(console.log.mock.calls[0][0]).toMatch(/^Server error 404:/);
 });
 
-test.skip('Fails on missing page', () => {
-  return getDataFromApi(['ATP']).then(response => {
-    expect(response.data).toBeFalsey;
-    expect(response.status).toBe(404);
-  });
-});
+test("Request canceled", async () => {
+  axios.get.mockRejectedValue({});
+  axios.isCancel.mockResolvedValue(true);
 
-test.skip('fake test', () => {
-  getSearchObject(['ATP', 'Escherichia coli']);
-  expect(true).toBeTruthy;
+  console.log = jest.fn();
+  // const cancelTokenSource = axios.CancelToken.source();
+  const request = getDataFromApi(["status"], {});
+  // cancelTokenSource.cancel();
+  await request;
+  expect(console.log.mock.calls[0][0]).toMatch(/^Request 'status' cancelled/);
 });
--- src/services/RestApi.js
+++ src/services/RestApi.js
@@ -4,6 +4,7 @@ import { errorDialogRef } from "~/components/ErrorDialog/ErrorDialog";
 
 const ROOT_URL = process.env.REACT_APP_REST_SERVER;
 const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+const IS_TEST = process.env.NODE_ENV === "test";
 const DEFAULT_ERROR_MESSAGE = (
   <span>
     We&apos;re sorry our server could not complete your request. Please try
@@ -13,17 +14,30 @@ const DEFAULT_ERROR_MESSAGE = (
   </span>
 );
 
-function getDataFromApi(params, options = {}, errorMessage = null) {
+function getDataFromApi(
+  params,
+  options = {},
+  errorMessage = null,
+  dialogRef = null
+) {
   const url = ROOT_URL + params.join("/");
   return axios.get(url, options).catch(error => {
-    if (!axios.isCancel(error)) {
-      errorDialogRef.current.open(
+    if (axios.isCancel(error)) {
+      if (IS_DEVELOPMENT || IS_TEST) {
+        console.log("Request '" + params.join("/") + "' cancelled");
+      }
+    } else {
+      if (dialogRef == null) {
+        dialogRef = errorDialogRef;
+      }
+      dialogRef.current.open(
         <span className="dialog-message-container">
           {errorMessage && <span>{errorMessage}</span>}
           {DEFAULT_ERROR_MESSAGE}
         </span>
       );
-      if (IS_DEVELOPMENT) {
+
+      if (IS_DEVELOPMENT || IS_TEST) {
         const errorInfo = error.response.data;
         console.log(
           "Server error " + errorInfo.status + ": " + errorInfo.detail
