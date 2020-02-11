f6d21da6b8828a07df2c809f949e06139d3741d1
Jonathan Karr <jonrkarr@gmail.com>
2020-02-11 18:02:38 -0500

    working on test with both Jest and Cypress

--- integration-tests/tests/scenes/BiochemicalEntityDetails/Metabolite.test.js
+++ integration-tests/tests/scenes/BiochemicalEntityDetails/Metabolite.test.js
@@ -11,12 +11,40 @@ describe("Metabolite scene", function() {
 
   it("Metabolite scence with organism successfully loads", function() {
     const metabolite = "dTDP-D-Glucose";
-    const organism = "Escherichia coli"
+    const organism = "Escherichia coli";
     cy.visit("/metabolite/" + metabolite + "/" + organism);
     cy.get(".page-title").should($el => {
       expect($el.text().startsWith("Metabolite: ")).to.be.true;
+      expect($el.text().endsWith(" in " + organism)).to.be.true;
     });
+  });
+});
+
+describe("Common components", function() {
+  it("Metadata section mounts and unmounts", function() {
+    const metabolite = "dTDP-D-Glucose";
+    const organism = "Escherichia coli";
+
+    cy.visit("/metabolite/" + metabolite);
     cy.get(".page-title").should($el => {
+      expect($el.text().startsWith("Metabolite: ")).to.be.true;
+    });
+
+    cy.visit("/metabolite/" + metabolite + "#description");
+    cy.get(".page-title").should($el => {
+      expect($el.text().startsWith("Metabolite: ")).to.be.true;
+    });
+
+    cy.visit("/metabolite/" + metabolite + "/" + organism);
+    cy.get(".page-title").should($el => {
+      expect($el.text().startsWith("Metabolite: ")).to.be.true;
+      expect($el.text().endsWith(" in " + organism)).to.be.true;
+    });
+
+    cy.visit("/metabolite/" + metabolite);
+    cy.visit("/metabolite/" + metabolite + "/" + organism);
+    cy.get(".page-title").should($el => {
+      expect($el.text().startsWith("Metabolite: ")).to.be.true;
       expect($el.text().endsWith(" in " + organism)).to.be.true;
     });
   });
