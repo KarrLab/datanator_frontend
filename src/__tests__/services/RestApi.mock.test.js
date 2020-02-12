9fbfa1e13901c52e589f5f0c245e9249dcc134a8
Jonathan Karr <jonrkarr@gmail.com>
2020-02-12 10:27:28 -0500

    updating and adding rest API tests

--- src/__tests__/services/RestApi.mock.test.js
+++ src/__tests__/services/RestApi.mock.test.js
@@ -1,4 +1,4 @@
-import { getDataFromApi } from "~/services/RestApi";
+import { getDataFromApi, genApiErrorHandler } from "~/services/RestApi";
 import axios from "axios";
 import {
   errorDialog,
@@ -8,43 +8,49 @@ import { render } from "@testing-library/react";
 
 jest.mock("axios");
 
-/* global jest, test, expect */
+/* global jest, describe, it, expect */
 
-test("Request successful", async () => {
-  axios.get.mockResolvedValue({
-    status: 200,
-    data: { message: "Api server is up." }
+describe("With mocked API calls", () => {
+  it("Request successful", async () => {
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: { message: "Api server is up." }
+    });
+
+    await getDataFromApi(["status"]).then(response => {
+      expect(response.status).toBe(200);
+      expect(response.data.message).toBe("Api server is up.");
+    });
   });
 
-  await getDataFromApi(["status"]).then(response => {
-    expect(response.status).toBe(200);
-    expect(response.data.message).toBe("Api server is up.");
+  it("Request failed", async () => {
+    axios.get.mockRejectedValue({
+      response: { status: 404, data: { status: 404, detail: "Server error." } }
+    });
+
+    render(errorDialog);
+    console.log = jest.fn();
+    const customErrMsg = "Custom error message";
+    await getDataFromApi(["not-implemented"], {}).catch(
+      genApiErrorHandler(["not-implemented"], customErrMsg)
+    );
+    expect(console.log.mock.calls[0][0]).toMatch(/^Server error 404:/);
+
+    // close error dialog
+    errorDialogRef.current.close();
   });
-});
 
-test("Request failed", async () => {
-  axios.get.mockRejectedValue({
-    response: { status: 404, data: { status: 404, detail: "Server error." } }
+  it("Request canceled", async () => {
+    axios.get.mockRejectedValue({});
+    axios.isCancel.mockResolvedValue(true);
+
+    console.log = jest.fn();
+    let cancelTokenSource = { token: 0, cancel: () => {} };
+    const request = getDataFromApi(["status"], {
+      cancelToken: cancelTokenSource.token
+    }).catch(genApiErrorHandler(["status"]));
+    cancelTokenSource.cancel();
+    await request;
+    expect(console.log.mock.calls[0][0]).toMatch(/^Request 'status' cancelled/);
   });
-
-  render(errorDialog);
-  console.log = jest.fn();
-  const customErrMsg = "Custom error message";
-  await getDataFromApi(["not-implemented"], {}, customErrMsg, errorDialogRef);
-  expect(console.log.mock.calls[0][0]).toMatch(/^Server error 404:/);
-
-  // close error dialog
-  errorDialogRef.current.close();
-});
-
-test("Request canceled", async () => {
-  axios.get.mockRejectedValue({});
-  axios.isCancel.mockResolvedValue(true);
-
-  console.log = jest.fn();
-  // const cancelTokenSource = axios.CancelToken.source();
-  const request = getDataFromApi(["status"], {});
-  // cancelTokenSource.cancel();
-  await request;
-  expect(console.log.mock.calls[0][0]).toMatch(/^Request 'status' cancelled/);
 });
---
+++ src/__tests__/services/RestApi.real.test.js
@@ -0,0 +1,21 @@
+import { getDataFromApi, genApiErrorHandler } from "~/services/RestApi";
+import axios from "axios";
+
+/* global jest, describe, it, expect */
+
+describe("With real API calls", () => {
+  it("Request canceled", async () => {
+    console.log = jest.fn();
+    let cancelTokenSource = axios.CancelToken.source();
+    const request = getDataFromApi(["status"], {
+      cancelToken: cancelTokenSource.token
+    })
+      .catch(genApiErrorHandler(["status"]))
+      .finally(() => {
+        cancelTokenSource = null;
+      });
+    cancelTokenSource.cancel();
+    await request;
+    expect(console.log.mock.calls[0][0]).toMatch(/^Request 'status' cancelled/);
+  });
+});
