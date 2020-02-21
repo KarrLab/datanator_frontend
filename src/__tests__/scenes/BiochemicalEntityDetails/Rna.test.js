277a3a6b3e4c9f89d4a136abe67599cd0ae7842d
yosefdroth <yosefdroth@gmail.com>
2020-02-21 12:49:18 -0500

    rna tests

---
+++ src/__tests__/scenes/BiochemicalEntityDetails/Rna.test.js
@@ -0,0 +1,37 @@
+import { HalfLifeDataTable } from "~/scenes/BiochemicalEntityDetails/Rna/HalfLifeDataTable";
+import testRawData from "~/__tests__/fixtures/rna-abundances-phophofructokinase";
+
+/* global describe, it, expect */
+describe("Reaction data page", () => {
+  it("Gets correct reaction data url", () => {
+    const entity =
+      "ATP-dependent 6-phosphofructokinase (ATP-PFK) (Phosphofructokinase) (EC 2.7.1.11) (Phosphohexokinase)";
+    // instantiate data table
+    const dataTable = new HalfLifeDataTable();
+
+    // assert URL correct
+    expect(dataTable.getUrl(entity)).toEqual(
+      "/rna/halflife/get_info_by_protein_name/?protein_name=ATP-dependent 6-phosphofructokinase (ATP-PFK) (Phosphofructokinase) (EC 2.7.1.11) (Phosphohexokinase)&_from=0&size=1000"
+    );
+  });
+
+  it("Formats concentration data correct", async () => {
+    // instantiate data table
+    const dataTable = new HalfLifeDataTable();
+
+    // format raw data
+    const formattedData = dataTable.formatData(testRawData);
+
+    // test formatted data
+    expect(formattedData).toHaveLength(3);
+
+    let formatedDatum = formattedData[0];
+    expect(formatedDatum).toEqual({
+      halfLife: 4256.48322,
+      organism: "Methanosarcina acetivorans",
+      growthMedium: "MeOH",
+      source: "10.1186/s12864-016-3219-8"
+    });
+    expect(formattedData[1].organism).toEqual("Methanosarcina acetivorans");
+  });
+});
