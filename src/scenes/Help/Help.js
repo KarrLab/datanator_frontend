3ca306fe06a2e3e6dd34514e994274eb4e6c079d
yosefdroth <yosefdroth@gmail.com>
2020-02-27 09:21:47 -0500

    tutorial

--- src/scenes/Help/Help.js
+++ src/scenes/Help/Help.js
@@ -185,171 +185,190 @@ class Help extends Component {
             <div className="content-block section" id="walkthrough">
               <h2 className="content-block-heading">Walkthrough</h2>
               <div className="content-block-content">
-                <ol className="tutorial">
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      In a seperate window, open up the home page. In the search
-                      bar, enter ATP and Bacillus subtilis subs subtilis.
-                    </div>
-                    <div className="tutorial-step-text">
-                       This will lead to a search results page in step 2. In this tutorial, we will be using ATP in Bacillus subtilis subs subtilis as an example. This process can be replicated with your biomolecule of interest.
-                    </div>
-                    <object
-                      data={one_home}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
+                <div className="walkthrough">
+                  <div className="overview-heading">Walkthrough overview</div>
+                  <div className="overview">
+                    Datanator can be used to systematically discover data for
+                    modeling a specific cell in a specific environment. In this
+                    walkthrough, we will be looking up data for ATP in Bacillus subtilis subs subtilis 
+                    The steps used in this tutorial can be replicated with any biomolecule
+                    of interest in any other organism or environment.
+                  </div>
+                  <ol className="tutorial">
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        In a seperate window, open up the home page. Type "ATP" in the first search bar and type 
+                        Bacillus subtilis subs in the second search bar. Enter the search 
+                      </div>
+                      <div className="tutorial-step-text">
+                        This will lead to a search results page in step 2.
+                      </div>
+                      <object
+                        data={one_home}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
 
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      Click on Adenosine triphosphate in the Metabolites section
-                    </div>
-                    <div className="tutorial-step-text">
-                      The search results page organizes the results by biomolecule type. Currently, you can browse through metabolites, RNAs, proteins, and reactions. 
-                      Clicking on Adenosine triphosphate will bring you to the data page in step 3.
-                    </div>
-                    <object
-                      data={two_intermediate}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        Click on Adenosine triphosphate in the Metabolites
+                        section
+                      </div>
+                      <div className="tutorial-step-text">
+                        The search results page organizes the results by
+                        biomolecule type. Currently, you can browse through
+                        metabolites, RNAs, proteins, and reactions. Clicking on
+                        Adenosine triphosphate will bring you to the data page
+                        in step 3.
+                      </div>
+                      <object
+                        data={two_intermediate}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
 
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      Browse through the metadata and click on "Concdentration"
-                      or scroll down
-                    </div>
-                    <div className="tutorial-step-text">
-                      The top of the page has metadata about ATP. For now, let’s
-                      look at concentrations data. Either click on “concentration” link
-                      in the table of contents or scroll down.
-                    </div>
-                    <object
-                      data={three_metadata}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        Read the metadata. To view concentration data, click on
+                        "Concdentration" in the table of contents or scroll down
+                      </div>
+                      <div className="tutorial-step-text">
+                        The top of the page has metadata about ATP. You can browse the description, 
+                        synonyms, external database links, and related pathways. Scroll to the bottom of the page to get to step 4.
+                      </div>
+                      <object
+                        data={three_metadata}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
 
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      Browse the Data Table
-                    </div>
-                    <div className="tutorial-step-text">
-                      The table has data about ATP concentrations. Each row in
-                      this table corresponds to an experimental observation of
-                      ATP. The source column contains links to the data’s
-                      original data location. Note that the table includes
-                      observations about structurally similar metabolites as
-                      well.
-                      <ol>
-                        <li>Columns - choose which columns to display</li>
-                        <li>Filters - filter data on the table</li>
-                        <li>Stats - see summary statistics of the data</li>
-                        <li>Export - export the data to CSV or JSON</li>
-                      </ol>
-                    </div>
-                    <object
-                      data={four_datatable}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        Read the Data Table
+                      </div>
+                      <div className="tutorial-step-text">
+                        The table has data about ATP concentrations. Each row in
+                        this table corresponds to an experimental observation of
+                        ATP. The source column contains links to the data's online location. 
+                        Note that although this page is for ATP, the table contains data about structurally similar 
+                        molecules as well (e.g. Adenosine monophosphate). This is allow the user to filter to include/exlude
+                        structurally similar molecules. The filters will be explained in step 5.
+                        <ol>
+                          <li>Columns - choose which columns to display</li>
+                          <li>Filters - filter data on the table</li>
+                          <li>Stats - see summary statistics of the data</li>
+                          <li>Export - export the data to CSV or JSON</li>
+                        </ol>
+                      </div>
+                      <object
+                        data={four_datatable}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
 
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">Filter the data</div>
-                    <div className="tutorial-step-text">
-                      There are a number of possible filters. Let's say we used
-                      three filters:
-                      <ol>
-                        <li>
-                          Chemical similarity - you can filter by the structural
-                          similarity of the molecules to ATP. 1 would be
-                          structurally identical (only ATP), and 6.5 would
-                          include many structurally similar molecules. The score
-                          corresponds to a <a
-                      href={
-                        "http://openbabel.org/docs/dev/Features/Fingerprints.html"
-                      }
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      tanimoto similarity score
-                    </a>.
-                        </li>
-                        <li>
-                          Taxonomic distance - the initial search was for
-                          Bacillus subtilis subsp. subtilis. This filter lets
-                          you filter the data for taxonomic distance. Each node
-                          on the slider corresponds to a classification. Right
-                          now, the slider is set to include all data from the
-                          kingdom Bacteria.
-                        </li>
-                        <li>
-                          Media - you can filter by media type. Try "gutnick"
-                          for example.
-                        </li>
-                      </ol>
-                    </div>
-                    <object
-                      data={five_datatable}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      Select Specific Data Points and Analyze
-                    </div>
-                    <div className="tutorial-step-text">
-                      Click on "Stats" in the left-hand toolbar. This will show summary statistics of the total data. 
-                      You can also select specific data points by checking the boxes on the table, and the stats toolbar
-                      will display stats specific to the selected data. 
-                    </div>
-                    <object
-                      data={six_datatable}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      Export molecular data for model construction and
-                      validation.
-                    </div>
-                    <div className="tutorial-step-text">
-                      Click "CSV" in the upper right corner of the table. The data can also be exported as a JSON.
-                    </div>
-                    <object
-                      data={seven_datatable}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
-                  <li className="tutorial-step">
-                    <div className="tutorial-step-title">
-                      View the data in excel. 
-                    </div>
-                    <div className="tutorial-step-text">
-                      The data can now be saved.
-                    </div>
-                    <object
-                      data={eight_excel}
-                      className="section-column-icon"
-                      alt="Identify parameters icon"
-                      aria-label="Identify parameters icon"
-                    />
-                  </li>
-                </ol>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">Filter the data</div>
+                      <div className="tutorial-step-text">
+                        The data can be filtered by similar entities (e.g., by structural similarity) relatedness of the organisms 
+                         (by phylogenetic distance) and by similar environments 
+                        (e.g., temperature, pH). In this walkthrough, we will use three filters:
+                        <ol>
+                          <li>
+                            Chemical similarity - you can filter by the
+                            structural similarity of the molecules to ATP. 1
+                            would be structurally identical (only ATP), and 6.5
+                            would include many structurally similar molecules.
+                            The score corresponds to a{" "}
+                            <a
+                              href={
+                                "http://openbabel.org/docs/dev/Features/Fingerprints.html"
+                              }
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              tanimoto similarity score
+                            </a>
+                            .
+                          </li>
+                          <li>
+                            Taxonomic distance - the initial search was for
+                            Bacillus subtilis subsp. subtilis. This filter lets
+                            you filter the data for taxonomic distance. Each
+                            node on the slider corresponds to a classification.
+                            Right now, the slider is set to include all data
+                            from the kingdom Bacteria.
+                          </li>
+                          <li>
+                            Media - you can filter by media type. Try "gutnick"
+                            for example.
+                          </li>
+                        </ol>
+                      </div>
+                      <object
+                        data={five_datatable}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        Select Specific Data Points and Analyze
+                      </div>
+                      <div className="tutorial-step-text">
+                        Click on "Stats" in the left-hand toolbar. This will
+                        show summary statistics of the total data, including the data that was filtered out in the previous step.
+                        Then click on the boxes on the table to select them. As the boxes are selected, the stats toolbar will display
+                        summary statistics of the selected data. This allows you to filter and then select specific data points, and then compare
+                        its distribution to the total data.
+                      </div>
+                      <object
+                        data={six_datatable}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        Export molecular data for model construction and
+                        validation.
+                      </div>
+                      <div className="tutorial-step-text">
+                        Click "CSV" in the upper right corner of the table. The
+                        data can also be exported as a JSON.
+                      </div>
+                      <object
+                        data={seven_datatable}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
+                    <li className="tutorial-step">
+                      <div className="tutorial-step-title">
+                        View the data in Excel.
+                      </div>
+                      <div className="tutorial-step-text">
+                        The data can now be saved.
+                      </div>
+                      <object
+                        data={eight_excel}
+                        className="section-column-icon"
+                        alt="Identify parameters icon"
+                        aria-label="Identify parameters icon"
+                      />
+                    </li>
+                  </ol>
+                </div>
               </div>
             </div>
           </div>
