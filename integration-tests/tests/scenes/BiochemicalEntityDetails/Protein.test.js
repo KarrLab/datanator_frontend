ce6224a74d158031e366ea8a261a3da4b9a1917a
yosefdroth <yosefdroth@gmail.com>
2020-04-13 17:02:06 -0400

    updading protein integration test

--- integration-tests/tests/scenes/BiochemicalEntityDetails/Protein.test.js
+++ integration-tests/tests/scenes/BiochemicalEntityDetails/Protein.test.js
@@ -33,7 +33,7 @@ describe("Protein scene", function() {
     // page title
     cy.get(".page-title").should(
       "have.text",
-      "Protein: Glucose-1-phosphate thymidylyltransferase in " + organism
+      "Gene: Glucose-1-phosphate thymidylyltransferase in " + organism
     );
 
     // data table
@@ -43,6 +43,52 @@ describe("Protein scene", function() {
       .first()
       .find(".ag-header-cell-text")
       .should("have.text", "Abundance");
+
+    // open columns tool panel and toggle all columns
+    cy.get("#" + dataContainerId + " .biochemical-entity-data-table-tool-panel")
+      .first()
+      .click();
+    cy.get(
+      "#" +
+        dataContainerId +
+        " .biochemical-entity-scene-columns-tool-panel input"
+    ).each($input => {
+      cy.wrap($input).click();
+    });
+
+    // open filters tool panel and open all filters
+    cy.get("#" + dataContainerId + " .biochemical-entity-data-table-tool-panel")
+      .eq(1)
+      .click();
+    cy.get(
+      "#" + dataContainerId + " .biochemical-entity-scene-filter-container"
+    ).each($filter => {
+      cy.wrap($filter).click();
+    });
+
+    // open stats tool panels
+    cy.get(
+      "#" + dataContainerId + " .biochemical-entity-data-table-tool-panel"
+    ).each($toolPanelButton => {
+      cy.wrap($toolPanelButton).click();
+    });
+  });
+
+  it("RNA scene with organism successfully loads", function() {
+    const route = "protein";
+    const entity = "K16370";
+    const organism = "Escherichia coli";
+    const url = "/" + route + "/" + entity + "/" + organism;
+
+    cy.visit(url);
+
+    // data table
+    const dataContainerId = "half-life";
+    cy.get("#" + dataContainerId + " .ag-root .ag-header-row")
+      .find(".ag-header-cell")
+      .first()
+      .find(".ag-header-cell-text")
+      .should("have.text", "Half-life (s-1)");
 
     // open columns tool panel and toggle all columns
     cy.get("#" + dataContainerId + " .biochemical-entity-data-table-tool-panel")
