df69934bad89037ad673c7b2ce8430f6716d71ab
Zhouyang Lian <zhouyang.lian@familian.life>
2020-05-18 11:19:45 -0400

    proper field names in each *SearchResultsList.js

--- src/scenes/BiochemicalEntityDetails/Metabolite/ReactionSearchResultsList.js
+++ src/scenes/BiochemicalEntityDetails/Metabolite/ReactionSearchResultsList.js
@@ -15,7 +15,7 @@ export default class ReactionSearchResultsList extends Component {
       pageCount * pageSize +
       "&size=" +
       pageSize +
-      "&fields=protein_name&fields=synonyms&fields=enzymes&fields=ko_name&fields=gene_name&fields=name&fields=enzyme_name&fields=product_names&fields=substrate_names&fields=enzymes.subunit.canonical_sequence&fields=species"
+      "&fields=rxn_id&fields=ec-code&fields=enzyme_names&fields=product_names&fields=products&fields=substrate_names&fields=substrates&fields=in_metabolites"
     );
   }
 
