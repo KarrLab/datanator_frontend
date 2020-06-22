d22237f5870f1262c8b23a1d0ae5a5bfffcd5ee5
Jonathan Karr <jonrkarr@gmail.com>
2020-06-22 10:07:39 -0400

    moving airbrake project id, key to CircleCI and Netlify environment variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,5 @@ fi
 npm run build
 
 curl -X POST -H "Content-Type: application/json" \
-    -d "{\"environment\":\"production\",\"username\":\"karr.lab.daemon\",\"repository\":\"https://github.com/KarrLab/datanator_frontend\",\"revision\":\"${COMMIT_REF}\"}" \
-    "https://airbrake.io/api/v4/projects/275996/deploys?key=527a182e6ad1d36557df055c2d85df51"
+    -d "{\"environment\":\"${NODE_ENV}\",\"username\":\"${BUILD_USERNAME}\",\"repository\":\"https://github.com/KarrLab/datanator_frontend\",\"revision\":\"${COMMIT_REF}\"}" \
+    "https://airbrake.io/api/v4/projects/${AIRBRAKE_PROJECT_ID}/deploys?key=${AIRBRAKE_PROJECT_KEY}"
