fe47f51398c686ee6d7bd49ee21f2561768a952c
Jonathan Karr <jonrkarr@gmail.com>
2020-06-22 16:09:33 -0400

    debugging integration test

--- integration-tests/tests/scenes/SearchResults.test.js
+++ integration-tests/tests/scenes/SearchResults.test.js
@@ -143,7 +143,7 @@ describe("Search results scene with more results", function () {
     cy.get("#metabolites .more-search-results-button").first().click();
 
     // appended metabolite results
-    cy.get("#metabolites .search-results-list > li").should("have.length", 20);
+    cy.get("#metabolites .search-results-list > li").should("have.length", 30);
 
     // request more genes
     cy.get("#genes .more-search-results-button").first().click();
