b60b1c0fe6eb42d167f9bdd981933f80a7bfe2ac
Jonathan Karr <jonrkarr@gmail.com>
2020-07-06 15:33:29 -0400

    handling searches via InChI representations

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -167,7 +167,7 @@ function parseHistoryLocationPathname(history) {
   if (match) {
     route = match[2];
     query = match[4] ? decodeURIComponent(match[4].trim()) || null : null;
-    organism = match[6] ? decodeURI(match[6].trim()) || null : null;
+    organism = match[6] ? decodeURIComponent(match[6].trim()) || null : null;
   }
   if (typeof organism === "string" && organism.toLowerCase() === "null") {
     organism = null;
