bf20e1d569cf4442c70fe091435da8e7bbfbc3f5
Jonathan Karr <jonrkarr@gmail.com>
2020-07-06 18:07:28 -0400

    updating dependencies

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,19 +1,16 @@
 import React from "react";
-import LazyLoad from "react-lazyload";
 import "./Logo.scss";
 import logo from "./logo.svg";
 
 const Logo = () => {
   return (
     <div className="logo">
-      <LazyLoad>
-        <object
-          data={logo}
-          className="logo-image no-pointer-events"
-          alt="Datanator logo"
-          aria-label="Datanator logo"
-        />
-      </LazyLoad>
+      <object
+        data={logo}
+        className="logo-image no-pointer-events"
+        alt="Datanator logo"
+        aria-label="Datanator logo"
+      />
     </div>
   );
 };
